2a4af21c023e205ab431051e7ac28f6e6f11cecb
balakrishnanrsss <balakrishnan.rajendran@accionlabs.com>
2024-07-10 10:11:04 +0530

    Update with correct routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.com
 
 const routes: Routes = [
   { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
-  { path: 'tutorials', component: TutorialsListComponent },
-  { path: 'tutorials/:id', component: TutorialDetailsComponent },
+  { path: 'testcases', component: TutorialsListComponent },
+  { path: 'testcases/:id', component: TutorialDetailsComponent },
   { path: 'add', component: AddTutorialComponent }
 ];
 
